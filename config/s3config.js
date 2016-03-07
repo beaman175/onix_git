ff8bd1758b6135e42a4a11f70c5886335213781e
beaman175 <dhyun175@gmail.com>
2016-03-07 19:43:34 +0900

    Delete s3config.js

--- config/s3config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "key": "AKIAIO57GU4QV3F6XJWQ",
-  "secret": "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",
-  "region": "ap-northeast-2",
-  "bucket": "onixs3",
-  "imageDir": "test",
-  "imageACL": "public-read" //권한
-};
