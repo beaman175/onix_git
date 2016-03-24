909030b378844abae9d4f31820555d3e54ace302
Choi Donghyun <dhyun175@gmail.com>
2016-03-24 13:29:55 +0900

    나머지 항목 모두 환경변수로 처리

--- config/s3config.js
+++ config/s3config.js
@@ -1,8 +1,8 @@
 module.exports = {
   "key": process.env.FMS_S3_KEY,
   "secret": process.env.FMS_S3_SECRET,
-  "region": "ap-northeast-2",
-  "bucket": "onixs3",
-  "imageDir": "onix",
-  "imageACL": "public-read" //권한
+  "region": process.env.FMS_S3_REGION,
+  "bucket": process.env.FMS_S3_BUCKET,
+  "imageDir": process.env.FMS_S3_DIR,
+  "imageACL": process.env.FMS_S3_ACL //권한
 };
