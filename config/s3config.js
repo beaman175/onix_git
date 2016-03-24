7e2e7788111a253f2a93d2faa97a9f0e5c4ee2cd
Choi Donghyun <dhyun175@gmail.com>
2016-03-24 13:33:46 +0900

    s3config

--- config/s3config.js
+++ config/s3config.js
@@ -1,3 +1,9 @@
-/**
- * Created by skplanet on 2016-03-24.
- */
+module.exports = {
+  "key": process.env.FMS_S3_KEY,
+  "secret": process.env.FMS_S3_SECRET,
+  "region": process.env.FMS_S3_REGION,
+  "bucket": process.env.FMS_S3_BUCKET,
+  "imageDir": process.env.FMS_S3_DIR,
+  "imageACL": process.env.FMS_S3_ACL //권한
+};
+
