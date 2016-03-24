cd3b022a1039495454c40e240e07b794497c94fb
beaman175 <dhyun175@gmail.com>
2016-03-24 13:19:15 +0900

    Delete s3config.js

--- config/s3config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "key": process.env.FMS_S3_KEY,
-  "secret": process.env.FMS_S3_SECRET,
-  "region": "ap-northeast-2",
-  "bucket": "onixs3",
-  "imageDir": "onix",
-  "imageACL": "public-read" //권한
-};
